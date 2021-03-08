e1b6c78e2bcd83eff0df07213e17abea2f1fb023
wsy <18611031643.163.com>
2021-03-08 14:56:31 +0800

    完成评论

--- src/评论案例/01-结构布局.jsx
+++ src/评论案例/01-结构布局.jsx
@@ -1,24 +1,85 @@
 import React from  'react'
 import './index.css'
 class Comment extends React.Component {
+    state = {
+        commentList: [],
+        user: '',
+        userComment: ''
+    }
+    // 渲染结构方法
+    renderList() {
+        const { commentList } = this.state
+        if (commentList.length === 0) {
+            return (
+                <div className='no'>没有评论快去添加吧~~~</div>
+            )
+        }
+        return (
+            <ul>
+                {
+                    commentList.map(item => (
+                        <li key={item.id}>
+                            <h4>评论人：{ item.name }</h4>
+                            <p>评论内容：{ item.comment }</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+    }
+    handle = (e) => {
+        const target = e.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+            [name]: value
+        })
+    }
+
+    publish = () => {
+        const { commentList, user, userComment } = this.state
+        if (user.trim() === '') {
+            alert('请输入用户名')
+            return
+        }
+        if (userComment.trim() === '') {
+            alert('请输入内容')
+            return
+        }
+        const newComment = [
+            {
+                id: Math.random(),
+                name: user,
+                comment: userComment
+            },
+            ...commentList
+        ]
+        this.setState({
+            commentList: newComment,
+            user: '',
+            userComment: ''
+        })
+    }
     render() {
         return (
             <div className="common">
                 {/* 评论输入框 */}
                 <div>
-                    <input type="text" placeholder='请输评论人' />
+                    <input name='user' value={this.state.user} onChange={this.handle} type="text" placeholder='请输评论人' />
                     <div>
-                      <textarea className='content' name="userComment" cols="30" rows="10" placeholder='请输入评论内容'></textarea>
+                        <textarea
+                            className='content'
+                            onChange={this.handle}
+                            value={this.state.userComment}
+                            name="userComment"
+                            cols="30" rows="10"
+                            placeholder='请输入评论内容'>
+                        </textarea>
                     </div>
-                    <button>发表评论</button>
+                    <button onClick={ this.publish }>发表评论</button>
                 </div>
                 {/* 评论列表 */}
-                <ul>
-                    <li>
-                        <h4>评论人：马克</h4>
-                        <p>评论内容：转圈圈</p>
-                    </li>
-                </ul>
+                { this.renderList() }
             </div>
         ) 
     }
