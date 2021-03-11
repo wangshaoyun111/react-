4d7ae311335b1c327be32d212f7bd6f4d9ff5c43
wsy <18611031643.163.com>
2021-03-11 10:14:16 +0800

    创建时钩子函数

---
+++ src/react生命周期/01-创建阶段的钩子函数.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+// 创建阶段 指 组件实例化
+export default class Handle extends React.Component {
+    // 构造函数  初始化state数据  为事件绑定this
+    constructor() {
+        super()
+        this.state = {}
+        console.dir(1);
+    }
+    // 钩子函数
+    //  组件完全渲染到浏览器以后才会触发
+    // 数据+模板 结合以后 生成DOM元素，将DOM元素完全插入到DOM结构中以后触发
+    // 1 执行dom 操作
+    // 2 发起axios 请求
+    componentDidMount() {
+        console.dir(3);
+        const box = document.getElementById('box')
+        console.log(box);
+    }
+    // render方法  渲染ui'结构的， 所以每次组件发生渲染都会执行render
+    // 注意：不能在里面调用  setState
+    render() {
+        console.dir(2);
+        return (
+            <div>
+                <h4 id='box'>我是创建阶段钩子函数</h4>
+            </div>
+        )
+    }
+}
